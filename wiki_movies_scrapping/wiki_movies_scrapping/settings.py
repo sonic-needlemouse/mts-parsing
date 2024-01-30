6d61cb54f9b25e15c26615dcb79c5893fa3318d4
Aleksei Kulikov <aleksei.kulikov@deecoob.com>
2024-01-30 09:38:03 +0100

    feature: change robot settings in settings file

--- wiki_movies_scrapping/wiki_movies_scrapping/settings.py
+++ wiki_movies_scrapping/wiki_movies_scrapping/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "wiki_movies_scrapping.spiders"
 #USER_AGENT = "wiki_movies_scrapping (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
