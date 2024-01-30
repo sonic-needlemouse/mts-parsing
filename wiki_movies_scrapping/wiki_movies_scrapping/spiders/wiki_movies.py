43b40f684e478307cbdcad0706923f2a34eff0b4
Aleksei Kulikov <aleksei.kulikov@deecoob.com>
2024-01-30 09:29:47 +0100

    feature: add additional functionality for the parser

--- wiki_movies_scrapping/wiki_movies_scrapping/spiders/wiki_movies.py
+++ wiki_movies_scrapping/wiki_movies_scrapping/spiders/wiki_movies.py
@@ -1,16 +1,22 @@
 import scrapy
 
+
 def parse_film_data(response):
     title = response.xpath('//*[@class="infobox-above"]//text()').getall()[-1]
     genre = response.xpath('//*[@data-wikidata-property-id="P136"]//text()').getall()
     director = response.xpath('//*[@data-wikidata-property-id="P57"]//text()').getall()
     country = response.xpath('//*[@data-wikidata-property-id="P495"]//text()').getall()
+    year = response.xpath(
+        '//*[@data-wikidata-property-id="P577"]//a[@title]//text() | '
+        '//*[@class="dtstart"]//text()'
+    ).getall()
 
     yield {
-        'Название': title,
-        'Жанр': genre,
-        'Режиссер': director,
-        'Страна': country,
+        "Название": title,
+        "Жанр": genre,
+        "Режиссер": director,
+        "Страна": country,
+        "Год": year,
     }
 
 
@@ -19,12 +25,15 @@ class WikiMoviesSpider(scrapy.Spider):
     allowed_domains = ["ru.wikipedia.org"]
     start_urls = ["https://ru.wikipedia.org/wiki/Категория:Фильмы_по_алфавиту"]
 
-
     def parse(self, response):
-        links = response.xpath('//div[@id="mw-pages"]//div[@class="mw-category-group"]//a/@href').getall()
+        links = response.xpath(
+            '//div[@id="mw-pages"]//div[@class="mw-category-group"]//a/@href'
+        ).getall()
         for link in links:
             yield response.follow(link, callback=parse_film_data)
 
-        next_page = response.xpath('//a[contains(text(), "Следующая страница")]/@href').extract_first()
+        next_page = response.xpath(
+            '//a[contains(text(), "Следующая страница")]/@href'
+        ).extract_first()
         if next_page:
             yield response.follow(next_page, callback=self.parse)
