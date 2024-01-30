130f86f92ef526690a64d3f7f7a77c328fc7ef7d
Aleksei Kulikov <aleksei.kulikov@deecoob.com>
2024-01-30 08:19:22 +0100

    feature: add a loop for the links parsing

--- wiki_movies_scrapping/wiki_movies_scrapping/spiders/wiki_movies.py
+++ wiki_movies_scrapping/wiki_movies_scrapping/spiders/wiki_movies.py
@@ -19,3 +19,12 @@ class WikiMoviesSpider(scrapy.Spider):
     allowed_domains = ["ru.wikipedia.org"]
     start_urls = ["https://ru.wikipedia.org/wiki/Категория:Фильмы_по_алфавиту"]
 
+
+    def parse(self, response):
+        links = response.xpath('//div[@id="mw-pages"]//div[@class="mw-category-group"]//a/@href').getall()
+        for link in links:
+            yield response.follow(link, callback=parse_film_data)
+
+        next_page = response.xpath('//a[contains(text(), "Следующая страница")]/@href').extract_first()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
